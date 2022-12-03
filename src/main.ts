6c425c7b5131d20815b2c72ca4c9b36553031057
Filip Kalný <totalolage@gmail.com>
2022-12-03 16:38:08 +0100

    Solved part 2

--- src/main.ts
+++ src/main.ts
@@ -31,41 +31,102 @@ const input = fs.readFileSync('input.txt', 'utf-8');
 
 const lines = input.split('\n');
 
+// const totalScore = lines.reduce((acc, line) => {
+//     const [them, us] = line.split(' ');
+
+//     const ourPlayScore = {
+//         X: 1,
+//         Y: 2,
+//         Z: 3,
+//     }[us];
+
+//     let ourWinScore = 0;
+//     // Draw
+//     if (
+//         (us === 'X' && them === 'A')
+//         || (us === 'Y' && them === 'B')
+//         || (us === 'Z' && them === 'C')
+//     ) {
+//         ourWinScore = 3;
+//     }
+//     // Win
+//     else if (
+//         (us === 'X' && them === 'C')
+//         || (us === 'Y' && them === 'A')
+//         || (us === 'Z' && them === 'B')
+//     ) {
+//         ourWinScore = 6;
+//     }
+//     // Else lost, score is 0
+
+//     const score = ourPlayScore + ourWinScore;
+//     acc += score;
+//     return acc;
+// }, 0);
+
+// console.log(totalScore);
+
+// --- Part Two ---
+// The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+// The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+// In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+// In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+// In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+// Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+// Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+
+enum RPS {
+    Rock = 'A',
+    Paper = 'B',
+    Scissors = 'C',
+}
+
+const RPS_score = {
+    [RPS.Rock]: 1,
+    [RPS.Paper]: 2,
+    [RPS.Scissors]: 3,
+}
+
+enum RES {
+    Lose = 'X',
+    Draw = 'Y',
+    Win = 'Z',
+}
+
+const RES_score = {
+    [RES.Lose]: 0,
+    [RES.Draw]: 3,
+    [RES.Win]: 6,
+}
+
 const totalScore = lines.reduce((acc, line) => {
-    const [them, us] = line.split(' ');
+    const [them, desiredGameResult] = line.split(' ') as [RPS, RES];
 
-    // Them: A = rock, B = paper, C = scissors
-    // Us: X = rock, Y = paper, Z = scissors
-    // Score: rock = 1, paper = 2, scissors = 3, lose = 0, draw = 3, win = 6
+    const us: RPS = {
+        // Lose
+        [RES.Lose]: {
+            [RPS.Rock]: RPS.Scissors,
+            [RPS.Paper]: RPS.Rock,
+            [RPS.Scissors]: RPS.Paper,
+        },
+        // Draw
+        [RES.Draw]: {
+            [RPS.Rock]: RPS.Rock,
+            [RPS.Paper]: RPS.Paper,
+            [RPS.Scissors]: RPS.Scissors,
+        },
+        // Win
+        [RES.Win]: {
+            [RPS.Rock]: RPS.Paper,
+            [RPS.Paper]: RPS.Scissors,
+            [RPS.Scissors]: RPS.Rock,
+        }
+    }[desiredGameResult][them];
 
-    const ourPlayScore = {
-        X: 1,
-        Y: 2,
-        Z: 3,
-    }[us];
-
-    let ourWinScore = 0;
-    // Draw
-    if (
-        (us === 'X' && them === 'A')
-        || (us === 'Y' && them === 'B')
-        || (us === 'Z' && them === 'C')
-    ) {
-        ourWinScore = 3;
-    }
-    // Win
-    else if (
-        (us === 'X' && them === 'C')
-        || (us === 'Y' && them === 'A')
-        || (us === 'Z' && them === 'B')
-    ) {
-        ourWinScore = 6;
-    }
-    // Else lost, score is 0
-
-    const score = ourPlayScore + ourWinScore;
-    acc += score;
-    return acc;
+    return acc + RPS_score[us] + RES_score[desiredGameResult];
 }, 0);
 
 console.log(totalScore);
