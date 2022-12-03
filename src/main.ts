5d4cf3b92acffb52f762a4e032d2fc20ae972762
Filip Kalný <totalolage@gmail.com>
2022-12-03 16:03:25 +0100

    Solved part 1

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,35 @@
+// 1000
+// 2000
+// 3000
+
+// 4000
+
+// 5000
+// 6000
+
+// 7000
+// 8000
+// 9000
+
+// 10000
+
+// This list represents the Calories of the food carried by five Elves.
+// In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
+// Find the Elf carrying the most Calories.How many total Calories is that Elf carrying ?
+
 import fs from 'fs';
 
-const file = fs.readFileSync('src/main.ts', 'utf-8');
+const list = fs.readFileSync('input.txt', 'utf-8');
+const lines = list.split('\n');
 
-console.log(file);
+const elves = lines.reduce((allElves, line) => {
+    if (line === '') {
+        return [...allElves, 0]
+    }
+
+    const calories = parseInt(line, 10);
+    const currentElf = allElves[allElves.length - 1];
+    return [...allElves.slice(0, -1), currentElf + calories];
+}, [0]);
+
+console.log(Math.max(...elves));
