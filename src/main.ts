8bf7ed02334a5b593b3b12564b2231fab38c07bd
Filip Kalný <totalolage@gmail.com>
2022-12-04 19:23:51 +0100

    Solved part 2

--- src/main.ts
+++ src/main.ts
@@ -53,19 +53,71 @@ const getItemPriority = (item: Item): number => {
     throw new Error(`Invalid item: ${item}`);
 };
 
-const totalPriority = input.split('\n').reduce((total, line) => {
-    const items = line.split('').filter(isValidItem);
+// const totalPriority = input.split('\n').reduce((total, line) => {
+//     const items = line.split('').filter(isValidItem);
 
-    const firstHalf = items.slice(0, items.length / 2);
-    const secondHalf = items.slice(items.length / 2);
+//     const firstHalf = items.slice(0, items.length / 2);
+//     const secondHalf = items.slice(items.length / 2);
 
-    const firstHalfItems = new Set(firstHalf);
-    const secondHalfItems = new Set(secondHalf);
+//     const firstHalfItems = new Set(firstHalf);
+//     const secondHalfItems = new Set(secondHalf);
 
-    const commonItems = new Set(Array.from(firstHalfItems).filter(item => secondHalfItems.has(item)));
-    const commonItemPriorities = Array.from(commonItems).map(getItemPriority);
+//     const commonItems = new Set(Array.from(firstHalfItems).filter(item => secondHalfItems.has(item)));
+//     const commonItemPriorities = Array.from(commonItems).map(getItemPriority);
 
-    return total + commonItemPriorities.reduce((sum, priority) => sum + priority, 0);
-}, 0);
+//     return total + commonItemPriorities.reduce((sum, priority) => sum + priority, 0);
+// }, 0);
+
+// console.log(totalPriority);
+
+// --- Part Two ---
+// As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+// For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+// The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+// Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+// Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+// vJrwpWtwJgWrhcsFMMfFFhFp
+// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+// PmmdzqPrVvPwwTWBwg
+// And the second group's rucksacks are the next three lines:
+
+// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+// ttgJtRGJQctTZtZT
+// CrZsJsPPZsGzwwsLwLmpwMDw
+// In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+// Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+// Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+
+const totalPriority = input.split('\n')
+    // Group lines by 3
+    .reduce((groupsOf3, line, index) => {
+        // Create new group if needed
+        if (index % 3 === 0) {
+            groupsOf3.push([] as any);
+        }
+
+        // Add line to group
+        groupsOf3[groupsOf3.length - 1].push(line);
+
+        return groupsOf3;
+    }, [] as [string, string, string][])
+    // Find common items
+    .reduce((total, [firstLine, secondLine, thirdLine]) => {
+        const firstItems = new Set(firstLine.split('').filter(isValidItem));
+        const secondItems = new Set(secondLine.split('').filter(isValidItem));
+        const thirdItems = new Set(thirdLine.split('').filter(isValidItem));
+
+        const commonItems = new Set(Array.from(firstItems).filter(item => secondItems.has(item) && thirdItems.has(item)));
+        const commonItemPriorities = Array.from(commonItems).map(getItemPriority);
+
+        return total + commonItemPriorities.reduce((sum, priority) => sum + priority, 0);
+    }, 0);
 
 console.log(totalPriority);
