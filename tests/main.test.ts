8a125f11c6f4efe379e2f974361180676ffd97f1
Filip Kalný <totalolage@gmail.com>
2022-12-09 15:12:03 +0100

    Solved part 1

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -6,7 +6,19 @@ describe('main', () => {
     });
 
     const testData = [
-        {input: 'This is a test', expected: 'This is a test'},
+        {
+            input: [
+                'R 4',
+                'U 4',
+                'L 3',
+                'D 1',
+                'R 4',
+                'D 1',
+                'L 5',
+                'R 2',
+            ].join('\n'),
+            expected: 13
+        },
     ]
 
     testData.forEach(({input, expected}) => {
